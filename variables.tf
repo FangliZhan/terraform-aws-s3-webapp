65c2a12274a3187608dd90b4832eb7cb969ccd44
FangliZhan <31291225+FangliZhan@users.noreply.github.com>
2022-04-14 14:52:39 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,12 @@
 variable "region" {}
 variable "prefix" {}
 variable "name" {}
+
+variable "aws_access_key_id" {
+  description = "default access key for service now with tfc"
+}
+
+variable "aws_secret_access_key" {
+  description = "default secret key for service now with tfc"
+}
+
